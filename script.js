d4bf21f8858ef4bc128f97faf5e6648c42872cf2
Aman Khan <amankhanaezakmi@gmail.com>
2023-11-17 11:26:55 +0530

    First Commit: pushing All the files.

---
+++ script.js
@@ -0,0 +1,14 @@
+// :root[data-theme="light"] {
+//     --text: #130613;
+//     --background: #ffffff;
+//     --primary: #d27dd4;
+//     --secondary: #e9bee9;
+//     --accent: #a337a4;
+//   }
+//   :root[data-theme="dark"] {
+//     --text: #f9ecf9;
+//     --background: #000000;
+//     --primary: #812b82;
+//     --secondary: #411641;
+//     --accent: #c65bc8;
+//   }
